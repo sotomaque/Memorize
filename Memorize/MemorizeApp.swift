49126d4a37d282e313dfb1090da02335447aa21d
sotomaque <sotomaque@gmail.com>
2021-09-21 18:27:10 -0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Enrique Sotomayor on 9/21/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
