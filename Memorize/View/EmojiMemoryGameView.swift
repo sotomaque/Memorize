6219f368210bde0086e23d668fee7737494532ed
sotomaque <sotomaque@gmail.com>
2021-09-27 14:46:15 -0700

    added pie shape

--- Memorize/View/EmojiMemoryGameView.swift
+++ Memorize/View/EmojiMemoryGameView.swift
@@ -50,6 +50,12 @@ struct CardView: View {
                 if card.isFaceUp {
                     shape.fill().foregroundColor(.white)
                     shape.strokeBorder(lineWidth: DrawingConstants.lineWidth)
+                    Pie(
+                        startAngle: Angle(degrees: 0-90),
+                        endAngle: Angle(degrees: 50-90)
+                    )
+                        .padding(DrawingConstants.piePadding)
+                        .opacity(DrawingConstants.pieOpacity)
                     Text(card.content).font(font(in: geometry.size))
                 } else if card.isMatched {
                     shape.opacity(0)
@@ -69,8 +75,9 @@ struct CardView: View {
     private struct DrawingConstants {
         static let cornerRadius: CGFloat = 10
         static let lineWidth: CGFloat = 3
-        static let fontScale: CGFloat = 0.75
-        
+        static let fontScale: CGFloat = 0.65
+        static let piePadding: CGFloat = 7
+        static let pieOpacity: CGFloat = 0.5
     }
 }
 
@@ -78,7 +85,9 @@ struct CardView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         let game = EmojiMemoryGame()
-        EmojiMemoryGameView(game: game)
+        game.choose(game.cards.first!)
+        
+        return EmojiMemoryGameView(game: game)
             .previewDevice("iPhone 12 mini")
     }
 }
---
+++ Memorize/View/Pie.swift
@@ -0,0 +1,38 @@
+//
+//  Pie.swift
+//  Memorize
+//
+//  Created by Enrique Sotomayor on 9/27/21.
+//
+
+import Foundation
+import SwiftUI
+
+struct Pie: Shape {
+    
+    var startAngle: Angle
+    var endAngle: Angle
+    var clockwise: Bool = false
+    
+    func path(in rect: CGRect) -> Path {
+        let center = CGPoint(x: rect.midX, y: rect.midY)
+        let radius = min(rect.height, rect.width) / 2
+        let start = CGPoint(
+            x: center.x + radius * CGFloat(cos(startAngle.radians)),
+            y: center.y + radius * CGFloat(sin(startAngle.radians))
+        )
+        
+        var p = Path()
+        
+        // move to center
+        p.move(to: center)
+        // draw line up to start point
+        p.addLine(to: start)
+        // go around arc
+        p.addArc(center: center, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: !clockwise)
+        // draw line back to start point
+        p.addLine(to: center)
+        
+        return p
+    }
+}
